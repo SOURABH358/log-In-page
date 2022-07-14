ef72efa42d3853e981abb10246842ed1856599d1
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-07-14 16:48:54 +0530

    added login

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT||3000
 
 app.use(express.static(path.join(__dirname,'project_1')))
 app.use(express.json())
